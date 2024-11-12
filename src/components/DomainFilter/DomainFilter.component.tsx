77c8c6055f2db880a2339bbdbeb53ce328a1ff6d
Théo Fargeas <theofargeas@MacBook-Pro-de-Theo.local>
2024-11-12 11:51:10 +0700

    [main]: Remove warning

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,71 +1,76 @@
-import { useEffect, useState } from 'react';
-import { threadId } from 'worker_threads';
+import { useEffect, useState } from "react";
 
 interface State {
-  countries: string[],
-  classifications: string[],
-  subClassifications: string[]
+  countries: string[];
+  classifications: string[];
+  subClassifications: string[];
 }
 
 interface Props {
-  domains?: string[]
+  domains?: string[];
 }
 
 const DomainFilter = (props: Props) => {
-  const domains = props?.domains ?? [];
-  const countries: string[] = [];
-  const classifications: string[] = [];
-  const subClassifications: string[] = [];
-  const s: any = {};
-
-  let [state, setState] = useState<State>({
+  const [state, setState] = useState<State>({
     countries: [],
     classifications: [],
-    subClassifications: []
+    subClassifications: [],
   });
 
-
   useEffect(() => {
-    for(let i = 0; i < domains.length; i++) {
-      if (countries.indexOf(domains[i].substring(0,2)) <= 0) {
-        countries.push(domains[i].substring(0,2))
+    const domains = props?.domains ?? [];
+    const countries: string[] = [];
+    const classifications: string[] = [];
+    const subClassifications: string[] = [];
+
+    for (let i = 0; i < domains.length; i++) {
+      if (countries.indexOf(domains[i].substring(0, 2)) <= 0) {
+        countries.push(domains[i].substring(0, 2));
       }
-      classifications.push(domains[i].substring(3,5));
+      classifications.push(domains[i].substring(3, 5));
       let flag = false;
-      for(let j = 0; j < subClassifications.length; j++) {
-        if (subClassifications[j] == domains[i].substring(6)) {
-          flag = true
+      for (let j = 0; j < subClassifications.length; j++) {
+        if (subClassifications[j] === domains[i].substring(6)) {
+          flag = true;
           break;
         }
       }
       if (!flag) {
         subClassifications.push(domains[i].substring(6));
       }
-    };
+    }
     setState({
       countries: countries,
       classifications: classifications.filter((e, i, l) => l.indexOf(e) === i),
-      subClassifications: subClassifications
+      subClassifications: subClassifications,
     });
-  }, [domains]);
+  }, [props?.domains]);
 
-  return (<>
-    <select name="countries" multiple>
-      {state.countries.map(country => (
-        <option value={country} key={country}>{country}</option>
-      ))}
-    </select>
-    <select name="classifications" multiple>
-      {state.classifications.map(classification => (
-        <option value={classification} key={classification}>{classification}</option>
-      ))}
-    </select>
-    <select name="subClassifications" multiple>
-      {state.subClassifications.map(subClassification => (
-        <option value={subClassification} key={subClassification}>{subClassification}</option>
-      ))}
-    </select>
-  </>);
-}
+  return (
+    <>
+      <select name="countries" multiple>
+        {state.countries.map((country) => (
+          <option value={country} key={country}>
+            {country}
+          </option>
+        ))}
+      </select>
+      <select name="classifications" multiple>
+        {state.classifications.map((classification) => (
+          <option value={classification} key={classification}>
+            {classification}
+          </option>
+        ))}
+      </select>
+      <select name="subClassifications" multiple>
+        {state.subClassifications.map((subClassification) => (
+          <option value={subClassification} key={subClassification}>
+            {subClassification}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
 
-export default DomainFilter
+export default DomainFilter;
